2d6179d8e0bb11294c3b96efcf4f8ee19c5c234b
MuthuKrishnan27 <muthukrishnan.office@gmail.com>
2023-07-20 08:47:52 +0530

    submitting code-Thu Jul 20 2023 08:47:50 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [text,setText] = useState("");
+  function change(event){
+    setText(event.target.value);
+  }
+  function check(){
+    if(text!==""){
+      return <p>Hello {text}!</p>
+    }
+  }
+  console.log(text);
   return (
     <div>
-        {/* Do not remove the main div */}
+        {/* Do not remove the main div */
+          <label>Enter your name : 
+          <input type="text" onChange={change}></input>
+          </label>
+        }
+        {check()}
     </div>
   )
 }
